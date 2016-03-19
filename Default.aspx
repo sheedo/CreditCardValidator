3bee9882a85af37cf406278e82303fb081bcf77f
sheedo <rasheed_andrews@live.com>
2016-03-19 10:17:37 -0500

    First Commit

---
+++ Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td>
+                   <asp:Label ID="lblCardNumber" runat="server" Text="Credit Card# : "></asp:Label>
+                </td>
+                <td>
+                   <asp:TextBox ID="txtCardNumber" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                   <asp:Label ID="lblResult" runat="server" Text="Result : "></asp:Label>
+                </td>
+                <td>
+                   <asp:TextBox ID="txtResult" runat="server" Enabled="false"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Button ID="btnProcess" runat="server" Text="Process" Width="217px" OnClick="btnProcessClick"/>
+                </td>
+            </tr>
+        </table>       
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    public void btnProcessClick(object sender, EventArgs e)
+    {
+        int digitSum = 0;
+        string digits = "";
+        string reversedCardNumber = "";
+        string cardNumber = txtCardNumber.Text;
+
+        // Remove spaces and reverse string
+        cardNumber = cardNumber.Replace(" ", null);
+        for (int i = cardNumber.Length - 1; i >= 0; i--)
+            reversedCardNumber += cardNumber[i];
+
+        // Double the digits in even-numbered positions
+        for (int i = 0; i < reversedCardNumber.Length; i++)
+        {
+            if ((i + 1) % 2 == 0)
+                digits += Convert.ToInt32(reversedCardNumber.Substring(i, 1)) * 2;
+            else
+                digits += reversedCardNumber.Substring(i, 1);
+        }
+
+        // Add the digits
+        for (int i = 0; i<digits.Length; i++)
+        digitSum += Convert.ToInt32(digits.Substring(i, 1));
+        // Check that the sum is divisible by 10
+        if ((digitSum % 10) == 0)
+            txtResult.Text = "TRUE";
+        else
+            txtResult.Text = "FALSE";
+    
+        }
+}
